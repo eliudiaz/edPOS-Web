290fceea462c845e080fd081e3bef283fe888c4a
Eliu Diaz <eliudiaz@gmail.com>
2018-03-01 12:11:23 -0600

    allow change price on sale module

--- app/Http/Controllers/SaleTempApiController.php
+++ app/Http/Controllers/SaleTempApiController.php
@@ -1,105 +1,109 @@
 <?php namespace App\Http\Controllers;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
 use App\SaleTemp;
-use App\Item;
-use \Auth, \Redirect, \Validator, \Input, \Session, \Response;
+use Auth;
 use Illuminate\Http\Request;
+use Input;
+use Redirect;
+use Response;
+use Session;
+use Validator;
 
-class SaleTempApiController extends Controller {
+class SaleTempApiController extends Controller
+{
 
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		
-		return Response::json(SaleTemp::with('item')->get());
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return view('sale.create');
-	}
+        return Response::json(SaleTemp::with('item')->get());
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		$SaleTemps = new SaleTemp;
-		$SaleTemps->item_id = Input::get('item_id');
-		$SaleTemps->cost_price = Input::get('cost_price');
-        $SaleTemps->selling_price = Input::get('selling_price');
-		$SaleTemps->quantity = 1;
-		$SaleTemps->total_cost = Input::get('cost_price');
-        $SaleTemps->total_selling = Input::get('selling_price');
-		$SaleTemps->save();
-		return $SaleTemps;
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return view('sale.create');
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		$SaleTemps = SaleTemp::find($id);
-        $SaleTemps->quantity = Input::get('quantity');
-        $SaleTemps->total_cost = Input::get('total_cost');
-        $SaleTemps->total_selling = Input::get('total_selling');
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return SaleTemp
+     */
+    public function store(Request $request)
+    {
+        $SaleTemps = new SaleTemp;
+        $SaleTemps->item_id = $request->get('item_id');
+        $SaleTemps->cost_price = $request->get('cost_price');
+        $SaleTemps->selling_price = $request->get('selling_price');
+        $SaleTemps->quantity = 1;
+        $SaleTemps->total_cost = $request->get('cost_price');
+        $SaleTemps->total_selling = $request->get('selling_price');
         $SaleTemps->save();
         return $SaleTemps;
-	}
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		SaleTemp::destroy($id);
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        $SaleTemps = SaleTemp::find($id);
+        $SaleTemps->quantity = $request->get('quantity');
+        $SaleTemps->selling_price = $request->get('selling_price');
+        $SaleTemps->total_cost = $request->get('total_cost');
+        $SaleTemps->total_selling = $request->get('total_selling');
+        $SaleTemps->save();
+        return $SaleTemps;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        SaleTemp::destroy($id);
+    }
 
 }
