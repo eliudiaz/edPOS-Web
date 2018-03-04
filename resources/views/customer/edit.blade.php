737d525e4c1bc60e79ddef6e65a235af8fe0a86f
Eliu Diaz <eliudiaz@gmail.com>
2018-03-04 07:51:38 -0600

    code formatte

--- resources/views/customer/edit.blade.php
+++ resources/views/customer/edit.blade.php
@@ -1,85 +1,84 @@
 @extends('app')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">{{trans('customer.update_customer')}}</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">{{trans('customer.update_customer')}}</div>
 
-				<div class="panel-body">
-					@if (Session::has('error_msg'))
-						<div class="alert alert-danger">
-							{{ Session::get('error_msg') }}
-						</div>
-					@endif
-					{!! Html::ul($errors->all()) !!}
+                    <div class="panel-body">
+                        @if (Session::has('error_msg'))
+                            <div class="alert alert-danger">
+                                {{ Session::get('error_msg') }}
+                            </div>
+                        @endif
+                        {!! Html::ul($errors->all()) !!}
 
-					{!! Form::model($customer, array('route' => array('customers.update', $customer->id), 'method' => 'PUT', 'files' => true)) !!}
+                        {!! Form::model($customer, array('route' => array('customers.update', $customer->id), 'method' => 'PUT', 'files' => true)) !!}
 
-					<div class="form-group">
-						{!! Form::label('account', trans('customer.account').' *') !!}
-						{!! Form::text('account', null, array('class' => 'form-control')) !!}
-					</div>
+                        <div class="form-group">
+                            {!! Form::label('account', trans('customer.account').' *') !!}
+                            {!! Form::text('account', null, array('class' => 'form-control')) !!}
+                        </div>
 
-					<div class="form-group">
-					{!! Form::label('name', trans('customer.name').' *') !!}
-					{!! Form::text('name', null, array('class' => 'form-control')) !!}
-					</div>
+                        <div class="form-group">
+                            {!! Form::label('name', trans('customer.name').' *') !!}
+                            {!! Form::text('name', null, array('class' => 'form-control')) !!}
+                        </div>
 
-					<div class="form-group">
-					{!! Form::label('email', trans('customer.email')) !!}
-					{!! Form::text('email', null, array('class' => 'form-control')) !!}
-					</div>
+                        <div class="form-group">
+                            {!! Form::label('email', trans('customer.email')) !!}
+                            {!! Form::text('email', null, array('class' => 'form-control')) !!}
+                        </div>
 
-					<div class="form-group">
-					{!! Form::label('phone_number', trans('customer.phone_number')) !!}
-					{!! Form::text('phone_number', null, array('class' => 'form-control')) !!}
-					</div>
+                        <div class="form-group">
+                            {!! Form::label('phone_number', trans('customer.phone_number')) !!}
+                            {!! Form::text('phone_number', null, array('class' => 'form-control')) !!}
+                        </div>
 
-					<div class="form-group">
-					{!! Form::label('avatar', trans('customer.choose_avatar')) !!}
-					{!! Form::file('avatar', null, array('class' => 'form-control')) !!}
-					</div>
+                        <div class="form-group">
+                            {!! Form::label('avatar', trans('customer.choose_avatar')) !!}
+                            {!! Form::file('avatar', null, array('class' => 'form-control')) !!}
+                        </div>
 
-					<div class="form-group">
-					{!! Form::label('addrees', trans('customer.address')) !!}
-					{!! Form::text('address', null, array('class' => 'form-control')) !!}
-					</div>
+                        <div class="form-group">
+                            {!! Form::label('addrees', trans('customer.address')) !!}
+                            {!! Form::text('address', null, array('class' => 'form-control')) !!}
+                        </div>
 
-					<div class="form-group">
-						{!! Form::label('discount_percentage', trans('customer.discount_percentage')) !!}
-						{!! Form::text('discount_percentage', null, array('class' => 'form-control')) !!}
-					</div>
+                        <div class="form-group">
+                            {!! Form::label('discount_percentage', trans('customer.discount_percentage')) !!}
+                            {!! Form::text('discount_percentage', null, array('class' => 'form-control')) !!}
+                        </div>
 
-					<div class="form-group">
-					{!! Form::label('city', trans('customer.city')) !!}
-					{!! Form::text('city', null, array('class' => 'form-control')) !!}
-					</div>
+                        <div class="form-group">
+                            {!! Form::label('city', trans('customer.city')) !!}
+                            {!! Form::text('city', null, array('class' => 'form-control')) !!}
+                        </div>
 
-					<div class="form-group">
-					{!! Form::label('state', trans('customer.state')) !!}
-					{!! Form::text('state', null, array('class' => 'form-control')) !!}
-					</div>
+                        <div class="form-group">
+                            {!! Form::label('state', trans('customer.state')) !!}
+                            {!! Form::text('state', null, array('class' => 'form-control')) !!}
+                        </div>
 
-					<div class="form-group">
-					{!! Form::label('zip', trans('customer.zip')) !!}
-					{!! Form::text('zip', null, array('class' => 'form-control')) !!}
-					</div>
+                        <div class="form-group">
+                            {!! Form::label('zip', trans('customer.zip')) !!}
+                            {!! Form::text('zip', null, array('class' => 'form-control')) !!}
+                        </div>
 
-					<div class="form-group">
-					{!! Form::label('company_name', trans('customer.company_name')) !!}
-					{!! Form::text('company_name', null, array('class' => 'form-control')) !!}
-					</div>
+                        <div class="form-group">
+                            {!! Form::label('company_name', trans('customer.company_name')) !!}
+                            {!! Form::text('company_name', null, array('class' => 'form-control')) !!}
+                        </div>
 
 
+                        {!! Form::submit(trans('customer.submit'), array('class' => 'btn btn-primary')) !!}
 
-					{!! Form::submit(trans('customer.submit'), array('class' => 'btn btn-primary')) !!}
-
-					{!! Form::close() !!}
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
